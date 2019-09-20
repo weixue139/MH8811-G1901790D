e703300432163c93e589f3b8795eaadb80809e94
WX <weixue139@msn.com>
2019-09-20 21:07:33 +0800

    add 2 assignment folders

---
+++ 01/Homework1_hello.py
@@ -0,0 +1,2 @@
+#python assignment homework1 by WEI XUE Matric G1901790D.
+print("Hello, world!")
---
+++ 02/Program1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 20:54:34 2019
+
+@author: WX
+"""
+
+#Assignment2 Program1 Modify Hello, world to ask user's name and output the name.
+name=input('What is your name?')
+print('Hello', name,'!')
